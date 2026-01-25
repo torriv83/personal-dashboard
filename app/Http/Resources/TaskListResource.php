3a4a8639ac0a02878dd3710b55cb42a33cda1aa5
Tor Rivera <tor@trivera.net>
2026-01-25 13:14:46 +0100

    fix(api): inkluder id i TaskListResource respons

--- app/Http/Resources/TaskListResource.php
+++ app/Http/Resources/TaskListResource.php
@@ -17,6 +17,7 @@ class TaskListResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'slug' => $this->slug,
             'name' => $this->name,
             'is_shared' => $this->is_shared,
